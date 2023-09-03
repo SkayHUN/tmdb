7a535771a6644a36033a0d87f26b9c1f2e807139
SkayHUN <seller.janos@gmail.com>
2023-09-03 09:37:01 +0200

    Remove caching and api call if there is no search query and hide notification box and pagination

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,24 +6,26 @@ class MoviesController < ApplicationController
     @page = params['page'] || 1
     cache_key = "#{@query}_#{@page}" 
 
-    if Rails.cache.fetch(cache_key).present?
-      @fetched_from = 'our server'
-      session[:cache_hit_counter] = session[:cache_hit_counter].to_i + 1 if session[:cache_hit_counter]
-    else
-      @fetched_from = '3rd party API'
-      session[:cache_hit_counter] = 0
-    end
+    if @query
+      if Rails.cache.fetch(cache_key).present?
+        @fetched_from = 'our server'
+        session[:cache_hit_counter] = session[:cache_hit_counter].to_i + 1 if session[:cache_hit_counter]
+      else
+        @fetched_from = '3rd party API'
+        session[:cache_hit_counter] = 0
+      end
 
-    fetched_data = Rails.cache.fetch(cache_key, expires_in: 2.minutes) do
-      MoviesClient.search(@query, @page.to_i)
-    end
+      fetched_data = Rails.cache.fetch(cache_key, expires_in: 2.minutes) do
+        MoviesClient.search(@query, @page.to_i)
+      end
 
-    @movies = fetched_data.results
+      @movies = fetched_data.results
+    end
 
     # for pagination
-    @total_pages = fetched_data.total_pages
-    @current_page = fetched_data.page
-    @total_results = fetched_data.total_results
+    @total_pages = fetched_data&.total_pages
+    @current_page = fetched_data&.page
+    @total_results = fetched_data&.total_results
 
     respond_to do |format|
       format.html
